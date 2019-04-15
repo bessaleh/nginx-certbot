3aed6a30dccbd5baf20ae810cf52145ea41fc5ce
bessaleh <br.essaleh@gmail.com>
2019-04-15 11:40:35 +0200

    Inverse email and staging params order

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -3,8 +3,9 @@
 domains=( "$@" )
 rsa_key_size=4096
 data_path="./data/certbot"
-email="$2" # Adding a valid address is strongly recommended
-staging=$3 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=$2 # Set to 1 if you're testing your setup to avoid hitting request limits
+email="$3" # Adding a valid address is strongly recommended
+
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
